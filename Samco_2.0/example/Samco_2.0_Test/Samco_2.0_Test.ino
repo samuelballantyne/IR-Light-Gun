b8b1440c2f8a76a0c359b96b0dd6d7d0eeaaa84a
Sam Ballantyne <43810437+samuelballantyne@users.noreply.github.com>
2020-03-18 13:16:53 +1300

    *Large update* added Samco 1.1 & Samco 2.0 code and schematics

---
+++ Samco_2.0/example/Samco_2.0_Test/Samco_2.0_Test.ino
@@ -0,0 +1,216 @@
+/*
+    This is an example Arduino code for DFRobot Positioning Infrared Camera ( SKU: SEN0158 ).
+    Basically, it's a camera that can "see" infrared lights and ascertain the coordinates of those lights, just like what's inside a Wiimote.
+    It can track up to four infrared lights at a time.
+    You can find more info on this sensor at https://www.dfrobot.com/index.php?route=product/product&product_id=1088
+    
+    WHAT THIS SKETCH DOES:
+    Outputs x and y coordinate values for four points to Serial out.
+    When a point is not detected ( blocked, out of range, etc ), the sensor outputs 1023 for both x and y values.
+    
+    HOOKUP:   
+            Arduino     Positioning IR Camera
+    ===============     =====================
+                 5V --> Red wire
+                GND --> Black wire
+    SDA (A4 on UNO) --> Yellow wire
+    SCL (A5 on UNO) --> Green wire
+    
+    MODIFICATION HISTORY:
+    // modified for use with SAMCO light guns by Sam Ballantyne Nov 2019.
+    // modified for http://DFRobot.com by kurakura, Jan 2015.
+    // modified for http://DFRobot.com by Lumi, Jan. 2014
+    // modified output for Wii-BlobTrack program by RobotFreak http://www.letsmakerobots.com/user/1433
+    // Wii Remote IR sensor  test sample code  by kako http://www.kako.com
+*/
+
+#include <Wire.h>
+#include <DFRobotIRPosition.h>
+
+DFRobotIRPosition myDFRobotIRPosition;         // declare a IRCam object
+
+int positionX[4];               // RAW Sensor Values
+int positionY[4];               
+
+int oneY = 0;                   // Re-mapped so left sensor is always read first
+int oneX = 0;
+int twoY = 0;
+int twoX = 0;
+
+int caliPin = 13;               // Set Calibration Pin
+int leftPin = 7;               // Set Left Mouse Pin
+int rightPin = A1;              // Set Right Mouse Pin
+int middlePin = A0;             // Set Middle Mouse Pin
+int pedalPin = 5;                // Set pedal Pin
+
+int caliButton = 0;
+int leftButton = 0;
+int rightButton = 0;
+int middleButton = 0;
+int pedalButton = 0;
+
+int buttonState1 = 0;           // Set Button states
+int lastButtonState1 = 0;
+int buttonState2 = 0;
+int lastButtonState2 = 0;
+int buttonState3 = 0;
+int lastButtonState3 = 0;
+int buttonState4 = 0;         
+int lastButtonState4 = 0;   
+int buttonState5 = 0;         
+int lastButtonState5 = 0;  
+
+void setup() {
+    digitalWrite (5, HIGH);                 // This pin is used to power the IR Camera when using Samco PCB
+    delay(500);
+    myDFRobotIRPosition.begin();       // initialize the object in setup()
+    Serial.begin(9600); 
+
+    pinMode(caliPin, INPUT_PULLUP);         // Set pin modes
+    pinMode(leftPin, INPUT_PULLUP);
+    pinMode(rightPin, INPUT_PULLUP);
+    pinMode(middlePin, INPUT_PULLUP);  
+    pinMode(pedalPin, INPUT_PULLUP);  
+}
+
+void loop() {
+  
+    getPosition();
+    mouseButtons();
+    pedalButtons();
+    PrintResults();
+
+    
+}
+
+void mouseButtons() {    // Setup Cali, Left, Right & Middle Mouse buttons
+  
+  buttonState1 = digitalRead(caliPin);
+  buttonState2 = digitalRead(leftPin);
+  buttonState3 = digitalRead(rightPin);
+  buttonState4 = digitalRead(middlePin);
+      
+
+  if (buttonState1 != lastButtonState1) {
+    if (buttonState1 == LOW) {
+      caliButton = 0;
+    }
+    else { 
+      caliButton = 255;
+    }
+    delay(10);
+  }
+  
+  if (buttonState2 != lastButtonState2) {
+    if (buttonState2 == LOW) {
+      leftButton = 0;
+    }
+    else { 
+      leftButton = 255;
+    }
+    delay(10);
+  }
+
+  if (buttonState3 != lastButtonState3) {
+    if (buttonState3 == LOW) {
+      rightButton = 0;
+    }
+    else { 
+      rightButton = 255;
+    }
+    delay(10);
+  }
+
+  if (buttonState4 != lastButtonState4) {     
+    if (buttonState4 == LOW) {
+      middleButton = 0;
+    }
+    else { 
+      middleButton = 255;
+    }
+    delay(10);
+  }
+  
+  lastButtonState1 = buttonState1;
+  lastButtonState2 = buttonState2;
+  lastButtonState3 = buttonState3;
+  lastButtonState4 = buttonState4;           
+}
+
+void pedalButtons() {    // Setup Cali, Left, Right & Middle Mouse buttons
+  
+  buttonState5 = digitalRead(pedalPin);
+
+    if (buttonState5 != lastButtonState5) {     
+    if (buttonState5 == LOW) {
+      pedalButton = 0;
+    }
+    else { 
+      pedalButton = 255;
+    }
+    delay(10);
+  }
+  
+  lastButtonState5 = buttonState5;            
+}
+
+void PrintResults() {    // Print results for debugging
+    Serial.print( oneX );
+    Serial.print( "," );
+    Serial.print( oneY );
+    Serial.print( "," );
+    Serial.print( twoX );
+    Serial.print( "," );
+    Serial.print( twoY );
+    Serial.print( "," );
+    Serial.print( positionX[2] );
+    Serial.print( "," );
+    Serial.print( positionY[2] );
+    Serial.print( "," );
+    Serial.print( positionX[3] );
+    Serial.print( "," );
+    Serial.print( positionY[3] );
+    Serial.print( "," );
+    Serial.print( caliButton );
+    Serial.print( "," );
+    Serial.print( leftButton );
+    Serial.print( "," );
+    Serial.print( rightButton );
+    Serial.print( "," );
+    Serial.print( middleButton );
+    Serial.print( "," );
+    Serial.print( pedalButton );
+    Serial.println();
+    delay( 20 );
+}
+
+void getPosition() {    // Get tilt adjusted position from IR postioning camera
+
+  myDFRobotIRPosition.requestPosition();
+
+  if (myDFRobotIRPosition.available()) {
+    for (int i = 0; i < 4; i++) {
+      positionX[i] = myDFRobotIRPosition.readX(i);
+      positionY[i] = map (myDFRobotIRPosition.readY(i), 0, 768, 768, 0 );
+    }
+
+      if (positionX[0] > positionX[1]) {
+      oneY = positionY[0];
+      oneX = positionX[0];
+      twoY = positionY[1];
+      twoX = positionX[1];
+    }
+    else if (positionX[0] < positionX[1]) {
+      oneY = positionY[1];
+      oneX = positionX[1];
+      twoY = positionY[0];
+      twoX = positionX[0];
+    }
+    else {
+      oneY = 1023;
+      oneX = 0;
+      twoY = 1023;
+      twoX = 0;
+    }
+  }
+}
---
+++ Samco_2.0/example/Samco_2.0_Windows/Samco_2.0_Windows.ino
@@ -0,0 +1,459 @@
+/*
+
+  Samco - Arduino Powered IR Light Gun
+
+  Go to https://github.com/samuelballantyne for code & instructions.
+
+  created June 2019
+  by Sam Ballantyne
+
+  This sample code is part of the public domain.
+
+*/
+
+int positionX[4];               // RAW Sensor Values
+int positionY[4];               
+
+int oneY = 0;                   // Re-mapped so left sensor is always read first
+int oneX = 0;
+int twoY = 0;
+int twoX = 0;
+
+int finalX = 0;                 // Values after tilt correction
+int finalY = 0;
+
+int xLeft = 0;                  // Stored calibration points
+int yTop = 0;
+int xRight = 0;
+int yBottom = 0;
+
+int MoveXAxis = 0;              // Unconstrained mouse postion
+int MoveYAxis = 0;               
+
+int conMoveXAxis = 0;           // Constrained mouse postion
+int conMoveYAxis = 0;           
+
+int count = 4;                  // Set intial count
+
+int _tigger = 7;               // Label Pin to buttons
+int _up = 11;                
+int _down = 9;              
+int _left = 10;             
+int _right = 12;               
+int _A = A1;                
+int _B = A0;              
+int _start = A2; 
+int _select = A3;               
+int _reload = 13;
+int _pedal = 5;               
+
+
+int buttonState1 = 0;           
+int lastButtonState1 = 0;
+int buttonState2 = 0;
+int lastButtonState2 = 0;
+int buttonState3 = 0;
+int lastButtonState3 = 0;
+int buttonState4 = 0;         
+int lastButtonState4 = 0; 
+int buttonState5 = 0;           
+int lastButtonState5 = 0;
+int buttonState6 = 0;
+int lastButtonState6 = 0;
+int buttonState7 = 0;
+int lastButtonState7 = 0;
+int buttonState8 = 0;         
+int lastButtonState8 = 0;          
+int buttonState9 = 0;           
+int lastButtonState9 = 0;
+int buttonState10 = 0;
+int lastButtonState10 = 0;    
+
+#include <HID.h>                // Load libraries
+#include <Wire.h>
+#include <Keyboard.h>
+#include <AbsMouse.h>
+#include <DFRobotIRPosition.h>
+
+DFRobotIRPosition myDFRobotIRPosition;
+
+int res_x = 1920;               // Put your screen resolution width here
+int res_y = 1080;               // Put your screen resolution height here
+
+
+void setup() {
+
+  Serial.begin(9600);                     // For debugging (make sure your serial monitor has the same baud rate)
+
+  AbsMouse.init(res_x, res_y);            
+
+  pinMode(_tigger, INPUT_PULLUP);         // Set pin modes
+  pinMode(_up, INPUT_PULLUP);
+  pinMode(_down, INPUT_PULLUP);
+  pinMode(_left, INPUT_PULLUP);
+  pinMode(_right, INPUT_PULLUP);         // Set pin modes
+  pinMode(_A, INPUT_PULLUP);
+  pinMode(_B, INPUT_PULLUP);
+  pinMode(_start, INPUT_PULLUP);  
+  pinMode(_select, INPUT_PULLUP);
+  pinMode(_reload, INPUT_PULLUP);       
+
+  myDFRobotIRPosition.begin();            // Start IR Camera
+
+  AbsMouse.move((res_x / 2), (res_y / 2));          // Set mouse position to centre of the screen
+  
+  delay(500);
+  
+}
+
+
+
+
+void loop() {
+
+  if (count > 3) {
+
+    getPosition();
+    mouseButtons();
+    PrintResults();
+    go();
+
+  }
+
+  /* ------------------ START/PAUSE MOUSE ---------------------- */
+
+
+  else if (count > 2 ) {
+
+    skip();
+    mouseCount();
+    mouseButtons();
+    PrintResults();
+
+  }
+
+
+  /* ---------------------- TOP LEFT --------------------------- */
+
+
+  else if (count > 1 ) {
+
+    AbsMouse.move(300, 200);
+
+    mouseCount();
+    getPosition();
+    reset();
+
+    xLeft = finalX;
+    yTop = finalY;
+
+    PrintResults();
+
+  }
+
+
+  /* -------------------- BOTTOM RIGHT ------------------------- */
+
+
+  else if (count > 0 ) {
+
+    AbsMouse.move((res_x - 300), (res_y - 200));
+
+    mouseCount();
+    getPosition();
+    reset();
+
+    xRight = finalX;
+    yBottom = finalY;
+
+    PrintResults();
+
+  }
+
+
+  /* ---------------------- LET'S GO --------------------------- */
+
+
+  else {
+
+    AbsMouse.move(conMoveXAxis, conMoveYAxis);
+
+    mouseButtons();
+    getPosition();
+
+    MoveXAxis = map (finalX, xLeft, xRight, 300, (res_x - 300));
+    MoveYAxis = map (finalY, yTop, yBottom, 200, (res_y - 200));
+    conMoveXAxis = constrain (MoveXAxis, 0, res_x);
+    conMoveYAxis = constrain (MoveYAxis, 0, res_y);
+
+    PrintResults();
+    reset();
+
+  }
+
+}
+
+
+/*        -----------------------------------------------        */
+/* --------------------------- METHODS ------------------------- */
+/*        -----------------------------------------------        */
+
+
+void getPosition() {    // Get tilt adjusted position from IR postioning camera
+
+  myDFRobotIRPosition.requestPosition();
+
+  if (myDFRobotIRPosition.available()) {
+    for (int i = 0; i < 4; i++) {
+      positionX[i] = myDFRobotIRPosition.readX(i);
+      positionY[i] = myDFRobotIRPosition.readY(i);
+    }
+
+        if (positionX[0] > positionX[1]) {
+      oneY = positionY[0];
+      oneX = positionX[0];
+      twoY = positionY[1];
+      twoX = positionX[1];
+    }
+    else if (positionX[0] < positionX[1]) {
+      oneY = positionY[1];
+      oneX = positionX[1];
+      twoY = positionY[0];
+      twoX = positionX[0];
+    }
+    else {
+      oneY = 1023;
+      oneX = 0;
+      twoY = 1023;
+      twoX = 0;
+    }
+    
+    finalX = 512 + cos(atan2(twoY - oneY, twoX - oneX) * -1) * (((oneX - twoX) / 2 + twoX) - 512) - sin(atan2(twoY - oneY, twoX - oneX) * -1) * (((oneY - twoY) / 2 + twoY) - 384);
+    finalY = 384 + sin(atan2(twoY - oneY, twoX - oneX) * -1) * (((oneX - twoX) / 2 + twoX) - 512) + cos(atan2(twoY - oneY, twoX - oneX) * -1) * (((oneY - twoY) / 2 + twoY) - 384);
+    
+  }
+
+  else {
+    Serial.println("Device not available!");
+  }
+
+}
+
+
+
+void go() {    // Setup Start Calibration Button
+
+  buttonState1 = digitalRead(_reload);
+
+  if (buttonState1 != lastButtonState1) {
+    if (buttonState1 == LOW) {
+      count--;
+    }
+    else { // do nothing
+    }
+    delay(50);
+  }
+  lastButtonState1 = buttonState1;
+}
+
+
+
+void mouseButtons() {    // Setup Left, Right & Middle Mouse buttons
+
+  buttonState2 = digitalRead(_tigger);
+  buttonState3 = digitalRead(_up);  
+  buttonState4 = digitalRead(_down);
+  buttonState5 = digitalRead(_left);
+  buttonState6 = digitalRead(_right);   
+  buttonState7 = digitalRead(_A);
+  buttonState8 = digitalRead(_B);
+  buttonState9 = digitalRead(_start);      
+  buttonState10 = digitalRead(_select); 
+
+  
+  if (buttonState2 != lastButtonState2) {
+    if (buttonState2 == LOW) {
+      AbsMouse.press(MOUSE_LEFT);
+    }
+    else {
+      AbsMouse.release(MOUSE_LEFT);
+    }
+    delay(10);
+  }
+
+  if (buttonState3 != lastButtonState3) {
+    if (buttonState3 == LOW) {
+    Keyboard.press(KEY_UP_ARROW);
+    delay(100);
+    Keyboard.releaseAll();
+    }
+    else {
+
+    }
+    delay(10);
+  }
+
+  if (buttonState4 != lastButtonState4) {     
+    if (buttonState4 == LOW) {
+    Keyboard.press(KEY_DOWN_ARROW);
+    delay(100);
+    Keyboard.releaseAll();
+    }
+    else {
+
+    }
+    delay(10);
+  }
+
+    if (buttonState5 != lastButtonState5) {
+    if (buttonState5 == LOW) {
+    Keyboard.press(KEY_LEFT_ARROW);
+    delay(100);
+    Keyboard.releaseAll();
+    }
+    else {
+ 
+    }
+    delay(10);
+  }
+  
+  if (buttonState6 != lastButtonState6) {
+    if (buttonState6 == LOW) {
+    Keyboard.press(KEY_RIGHT_ARROW);
+    delay(100);
+    Keyboard.releaseAll();
+    }
+    else {
+ 
+    }
+    delay(10);
+  }
+
+if (buttonState7 != lastButtonState7) {
+    if (buttonState7 == LOW) {
+      AbsMouse.press(MOUSE_RIGHT);
+    }
+    else {
+      AbsMouse.release(MOUSE_RIGHT);
+    }
+    delay(10);
+  }
+
+  if (buttonState8 != lastButtonState8) {     
+    if (buttonState8 == LOW) {
+      AbsMouse.press(MOUSE_MIDDLE);
+    }
+    else {
+      AbsMouse.release(MOUSE_MIDDLE);
+    }
+    delay(10);
+  }
+    if (buttonState9 != lastButtonState9) {
+    if (buttonState9 == LOW) {
+    Keyboard.press(KEY_BACKSPACE);
+    delay(100);
+    Keyboard.releaseAll();
+    }
+    else {
+ 
+    }
+    delay(10);
+  }
+  
+  if (buttonState10 != lastButtonState10) {
+    if (buttonState10 == LOW) {
+    Keyboard.press(KEY_RETURN);
+    delay(100);
+    Keyboard.releaseAll();
+    }
+    else {
+ 
+    }
+    delay(10);
+  }
+
+  lastButtonState2 = buttonState2;
+  lastButtonState3 = buttonState3;
+  lastButtonState4 = buttonState4;      
+  lastButtonState5 = buttonState5;
+  lastButtonState6 = buttonState6;
+  lastButtonState7 = buttonState7;
+  lastButtonState8 = buttonState8;
+  lastButtonState9 = buttonState9;
+  lastButtonState10 = buttonState10;      
+}
+
+
+void mouseCount() {    // Set count down on trigger
+
+  buttonState2 = digitalRead(_tigger);
+
+  if (buttonState2 != lastButtonState2) {
+    if (buttonState2 == LOW) {
+      count--;
+    }
+    else { // do nothing
+    }
+    delay(10);
+  }
+
+  lastButtonState2 = buttonState2;
+}
+
+
+void reset() {    // Pause/Re-calibrate button
+
+  buttonState1 = digitalRead(_reload);
+
+  if (buttonState1 != lastButtonState1) {
+    if (buttonState1 == LOW) {
+      count = 3;
+      delay(50);
+    }
+    else { // do nothing
+    }
+    delay(50);
+  }
+  lastButtonState1 = buttonState1;
+}
+
+
+void skip() {    // Unpause button
+
+  buttonState1 = digitalRead(_reload);
+
+  if (buttonState1 != lastButtonState1) {
+    if (buttonState1 == LOW) {
+      count = 0;
+      delay(50);
+    }
+    else { // do nothing
+    }
+    delay(50);
+  }
+  lastButtonState1 = buttonState1;
+}
+
+
+void PrintResults() {    // Print results for debugging
+
+  Serial.print("RAW: ");
+  Serial.print(finalX);
+  Serial.print(", ");
+  Serial.print(finalY);
+  Serial.print("     Count: ");
+  Serial.print(count);
+  Serial.print("     Calibration: ");
+  Serial.print(xLeft);
+  Serial.print(", ");
+  Serial.print(yTop);
+  Serial.print(", ");
+  Serial.print(xRight);
+  Serial.print(", ");
+  Serial.print(yBottom);
+  Serial.print("     Position: ");
+  Serial.print(conMoveXAxis);
+  Serial.print(", ");
+  Serial.println(conMoveYAxis);
+
+}
